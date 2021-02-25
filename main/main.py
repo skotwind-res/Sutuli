de2b02311708b9a0d52bd318a67ec90776314743
skotwind-res <skotwing.res.2@gmail.com>
2021-02-25 20:37:36 +0300

    add test dicts

--- main/main.py
+++ main/main.py
@@ -1,1 +1,17 @@
 print("it is work.")
+
+my_list = [1,2,3,4,5]
+
+my_dict_1 = {
+	'key_1': 1,
+	'key_2': 1,
+	'key_3': 1,
+}
+
+my_dict_2 = {
+	'key_1': 1,
+	'key_2': 1,
+	'key_3': 1
+}
+
+my_dict_3 = {'key_1': 1,'key_2': 1,'key_3': 1}
